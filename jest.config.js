cad4b5c1fc0f0f6f0e8956f541f97465722ee575
Tarcisio Vitor <tarcisiosouzabr@gmail.com>
2024-04-12 15:53:00 +0000

    add `jest.config` and `compose.yaml` database local ready

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+const dotEnv = requires("dotenv");
+dotEnv.config({ path: ".env.development" });
+const nextJest = require("next/jest");
+const createJestConfig = nextJest({ dir: "./" });
+const jestConfig = createJestConfig({
+  moduleDirectories: ["node_modules", "<rootDir>"],
+});
+module.exports = jestConfig;

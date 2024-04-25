081b70adb14ef2d15ee5169374cc12ca0209d78f
Tarcisio Vitor <tarcisiosouzabr@gmail.com>
2024-04-25 01:48:30 +0000

    Keyboard prototype on `Home/index` in `index.js`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,22 @@
+import styles from "styles/styles.module.css";
+
 function Home() {
-  return <h1>Welcome to Readmusics.com</h1>;
+  return (
+    <ul className={styles.set}>
+      <li className={[styles.key, styles.white, styles.c].join(" ")}></li>
+      <li className={[styles.key, styles.black, styles.cs].join(" ")}></li>
+      <li className={[styles.key, styles.white, styles.d].join(" ")}></li>
+      <li className={[styles.key, styles.black, styles.ds].join(" ")}></li>
+      <li className={[styles.key, styles.white, styles.e].join(" ")}></li>
+      <li className={[styles.key, styles.white, styles.f].join(" ")}></li>
+      <li className={[styles.key, styles.black, styles.fs].join(" ")}></li>
+      <li className={[styles.key, styles.white, styles.g].join(" ")}></li>
+      <li className={[styles.key, styles.black, styles.gs].join(" ")}></li>
+      <li className={[styles.key, styles.white, styles.a].join(" ")}></li>
+      <li className={[styles.key, styles.black, styles.as].join(" ")}></li>
+      <li className={[styles.key, styles.white, styles.b].join(" ")}></li>
+    </ul>
+  );
 }
 
 export default Home;

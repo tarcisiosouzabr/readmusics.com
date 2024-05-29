d0c0da3a6b485f85e7c4e969740c99a9bb03d3c6
Tarcisio Vitor <tarcisiosouzabr@gmail.com>
2024-05-29 17:32:19 +0000

    Handling clicks on `pages/index.js`

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,72 @@
 import styles from "styles/styles.module.css";
 
 function KeyBoard() {
+  function handleClick(e) {
+    const value = e.currentTarget.getAttribute("data-value");
+    console.log(value);
+  }
   return (
     <ul className={styles.set}>
-      <li className={[styles.key, styles.white, styles.c].join(" ")}></li>
-      <li className={[styles.key, styles.black, styles.cs].join(" ")}></li>
-      <li className={[styles.key, styles.white, styles.d].join(" ")}></li>
-      <li className={[styles.key, styles.black, styles.ds].join(" ")}></li>
-      <li className={[styles.key, styles.white, styles.e].join(" ")}></li>
-      <li className={[styles.key, styles.white, styles.f].join(" ")}></li>
-      <li className={[styles.key, styles.black, styles.fs].join(" ")}></li>
-      <li className={[styles.key, styles.white, styles.g].join(" ")}></li>
-      <li className={[styles.key, styles.black, styles.gs].join(" ")}></li>
-      <li className={[styles.key, styles.white, styles.a].join(" ")}></li>
-      <li className={[styles.key, styles.black, styles.as].join(" ")}></li>
-      <li className={[styles.key, styles.white, styles.b].join(" ")}></li>
+      <li
+        onClick={handleClick}
+        data-value="C"
+        className={[styles.key, styles.white, styles.c].join(" ")}
+      ></li>
+      <li
+        onClick={handleClick}
+        data-value="Cs"
+        className={[styles.key, styles.black, styles.cs].join(" ")}
+      ></li>
+      <li
+        onClick={handleClick}
+        data-value="D"
+        className={[styles.key, styles.white, styles.d].join(" ")}
+      ></li>
+      <li
+        onClick={handleClick}
+        data-value="Ds"
+        className={[styles.key, styles.black, styles.ds].join(" ")}
+      ></li>
+      <li
+        onClick={handleClick}
+        data-value="E"
+        className={[styles.key, styles.white, styles.e].join(" ")}
+      ></li>
+      <li
+        onClick={handleClick}
+        data-value="F"
+        className={[styles.key, styles.white, styles.f].join(" ")}
+      ></li>
+      <li
+        onClick={handleClick}
+        data-value="Fs"
+        className={[styles.key, styles.black, styles.fs].join(" ")}
+      ></li>
+      <li
+        onClick={handleClick}
+        data-value="G"
+        className={[styles.key, styles.white, styles.g].join(" ")}
+      ></li>
+      <li
+        onClick={handleClick}
+        data-value="Gs"
+        className={[styles.key, styles.black, styles.gs].join(" ")}
+      ></li>
+      <li
+        onClick={handleClick}
+        data-value="A"
+        className={[styles.key, styles.white, styles.a].join(" ")}
+      ></li>
+      <li
+        onClick={handleClick}
+        data-value="As"
+        className={[styles.key, styles.black, styles.as].join(" ")}
+      ></li>
+      <li
+        onClick={handleClick}
+        data-value="B"
+        className={[styles.key, styles.white, styles.b].join(" ")}
+      ></li>
     </ul>
   );
 }

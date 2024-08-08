0efbe66d08c2e99ff91802d0216dcfe8969ca958
Tarcisio Souza <tarcisiosouzabr@gmail.com>
2024-08-08 20:20:13 -0300

    feat: add `img` to `navBar.js`

--- components/navBar.js
+++ components/navBar.js
@@ -1,37 +1,67 @@
 import React from "react";
 import SessionButton from "components/sessionButton";
-
+import styles from "../styles/styles.module.css";
 export default function Navbar({ fixed }) {
-  const [navbarOpen, setNavbarOpen] = React.useState(false);
   return (
     <>
-      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-indigo-500 mb-3">
-        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
-          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-            <a
-              className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
-              href="#pablo"
-            >
-              Read Musics
-            </a>
+      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a
+            href="https://readmusics.com/"
+            className="flex items-center space-x-3 rtl:space-x-reverse"
+          >
+            <img className={styles.logoMain} alt="Flowbite Logo" />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              ReadMusics
+            </span>
+          </a>
+          <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
             <button
-              className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
               type="button"
-              onClick={() => setNavbarOpen(!navbarOpen)}
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
-              <i className="fas fa-bars"></i>
+              <SessionButton></SessionButton>
+            </button>
+            <button
+              data-collapse-toggle="navbar-cta"
+              type="button"
+              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="navbar-cta"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="w-5 h-5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 17 14"
+              >
+                <path stroke="currentColor" d="M1 1h15M1 7h15M1 13h15" />
+              </svg>
             </button>
           </div>
           <div
-            className={
-              "lg:flex flex-grow items-center" +
-              (navbarOpen ? " flex" : " hidden")
-            }
-            id="example-navbar-danger"
+            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+            id="navbar-cta"
           >
-            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
-              <li className="nav-item">
-                <SessionButton></SessionButton>
+            <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500"
+                  aria-current="page"
+                >
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Exercises
+                </a>
               </li>
             </ul>
           </div>
--- components/sessionButton.js
+++ components/sessionButton.js
@@ -4,31 +4,13 @@ export default function SessionButton() {
   if (session) {
     return (
       <>
-        <span className="leading-snug text-white text-xs uppercase font-bold ml-2">
-          {session.user.name}
-        </span>
-        <a
-          className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-          href="#pablo"
-          onClick={() => signOut()}
-        >
-          <i className="fab fa-facebook-square text-lg leading-lg text-white opacity-75"></i>
-
-          <span className="ml-2">Sign out</span>
-        </a>
+        <div onClick={() => signOut()}>{session.user.name}</div>
       </>
     );
   }
   return (
     <>
-      <a
-        className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-        href="#pablo"
-        onClick={() => signIn()}
-      >
-        <i className="fab fa-facebook-square text-lg leading-lg text-white opacity-75"></i>
-        <span className="ml-2">Sign in</span>
-      </a>
+      <div onClick={() => signIn()}>Log In</div>
     </>
   );
 }

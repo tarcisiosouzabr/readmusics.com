abec56d25b5a465cbacba4dde8ae64632e10a618
Tarcisio Souza <tarcisiosouzabr@gmail.com>
2024-09-05 23:05:21 -0300

    feat: add `sequelize` and migrations to `User` table

---
+++ config/config.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const dotEnv = require("dotenv");
+dotEnv.config({ path: ".env.development" });
+
+module.exports = {
+  development: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST,
+    port: 5432,
+    dialect: "postgres",
+  },
+  test: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST,
+    port: 5432,
+    dialect: "postgres",
+  },
+  production: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST,
+    port: 5432,
+    dialect: "postgres",
+  },
+};

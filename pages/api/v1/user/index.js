2fc152b7ce149ae590bf16d1fb476ca8c1cc9a22
Tarcisio Souza <tarcisiosouzabr@gmail.com>
2024-09-18 23:12:02 -0300

    chore: remove `sequelize` files

--- pages/api/v1/user/index.js
+++ pages/api/v1/user/index.js
@@ -1,16 +1,3 @@
-const { User } = require("../../../../models");
-
 export default async function user(request, response) {
-  const jane = await User.create({
-    firstName: "First Test Name",
-    lastName: "Last Test Name",
-    email: "testuser@test.com",
-  });
-  // const testUser = await models.create({
-  //   firstName: "First Test Name",
-  //   lastName: "Last Test Name",
-  //   email: "testuser@test.com",
-  // });
-
   response.status(200).json();
 }
